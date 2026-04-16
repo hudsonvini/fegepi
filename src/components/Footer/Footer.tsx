0231d948960c502ae407a5a392be944b543cd545
Hudson <hudsonvini26@gmail.com>
2026-04-16 11:51:24 -0300

    FEat: add new style on footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -60,18 +60,7 @@ export default function Footer({
             <div className={styles.shell}>
                 <div className={styles.content}>
                     <div className={styles.brandColumn}>
-                        <div className={styles.brandBadge}>
-                            <div className={styles.flag} aria-hidden="true">
-                                <span className={styles.star}>★</span>
-                                <span className={styles.flagTop}></span>
-                                <span className={styles.flagBottom}></span>
-                            </div>
-
-                            <div className={styles.brandCenter}>
-                                <strong>FEGEPI</strong>
-                                <span>FEDERAÇÃO DE GAMES E E-SPORTS DO PIAUÍ</span>
-                            </div>
-                        </div>
+                        <img src="/images/logoWhite.png" alt="" />
                     </div>
 
                     <div className={styles.linksGrid}>
@@ -100,7 +89,7 @@ export default function Footer({
                     </div>
 
                     <a className={styles.socialLink} href="#" aria-label="Instagram da FEGEPI">
-                        {/* <Instagram /> */}
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.3 141a115 115 0 1 0 -.6 230 115 115 0 1 0 .6-230zm-.6 40.4a74.6 74.6 0 1 1 .6 149.2 74.6 74.6 0 1 1 -.6-149.2zm93.4-45.1a26.8 26.8 0 1 1 53.6 0 26.8 26.8 0 1 1 -53.6 0zm129.7 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM399 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
                     </a>
 
                     <p>Federação de Games e E-Sports do Piauí @ Todos os direitos reservados</p>
